6fec77aff1cba75c96141033329dce8f98114a98
MyUsername <vanaliedoardo@gmail.com>
2022-07-04 17:51:23 +0200

    sezione in corso

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -1,14 +1,43 @@
 <template lang="">
-    <div class="fluid-container pt-5 pb-5 bg-newsletter text-center d-flex justify-content-center">
-        <div>Dig a Little Deeper  Into Damon Vaughn</div>
-   </div>
+    <div class="fluid-container py-5">
+        <div class="text-center">
+         <div class="fs-2 pb-3">Dig a Little Deeper  Into Damon Vaughn</div>
+          <div class="text-success pb-5">FIND OUT MORE ABOUT THE AUTHOR HIMSELF</div>
+        </div>
+   <div class="container d-flex">
+    <div v-for="(item,i) in AboutList"  :key="i" class="position-relative">
+   
+    <img :src="item.url" alt="" class="about-img w-75 h-100">
+    <div class="text-light pb-2 ">About Me</div>
     
+    </div>
+   </div>
+
+     </div>
 </template>
 <script>
 export default {
     data() {
         return {
+            AboutList: [
+                {
+                    url: 'img/box-1.jpg',
+                    titolo: 'About  Me',
+                    testo: "Vestibulum ante ipsum primis"
 
+                },
+                {
+                    url: 'img/box-3.jpg',
+                    titolo: 'My Lastes Book',
+                    testo: "Vestibulum ante ipsum primis"
+                },
+                {
+                    url: 'img/box-4.jpg',
+                    titolo: 'Book singing',
+                    testo: "Vestibulum ante ipsum primis"
+                },
+
+            ]
         }
     }
 }
@@ -16,4 +45,8 @@ export default {
 
 
 <style lang="scss">
+.about-img {
+    border-bottom-right-radius: 16%;
+    position: relative;
+}
 </style>
